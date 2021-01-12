168e25d0ea03abd07c33d3b39a85b75777ac2738
rajabca04 <rajabca201804@gmail.com>
2021-01-12 16:51:48 +0530

    2nd day

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>code with sadiq</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a href="" class="navbar-brand"></a>
+</nav>
+    
+</body>
+</html>
